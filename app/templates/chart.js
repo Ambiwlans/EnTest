7c0af6d856dcb6ac66e4136e874e2cb2a063cfad
Ambiwlans <udp.castellani@gmail.com>
2022-03-13 23:41:05 -0400

    milestone markers on main chart (pull from jk)

--- app/templates/chart.js
+++ app/templates/chart.js
@@ -1,9 +1,85 @@
 {% block chart %}
 <script>
+const verticalLinePlugin = {
+    renderVerticalLine: function (chartInstance, xVal, text) {
+        const yscale = chartInstance.scales['y-axis-0'];
+        const xscale = chartInstance.scales['x-axis-0'];
+        const context = chartInstance.chart.ctx;
+        const lineLeftOffset = xscale.getPixelForValue(xVal);
+        var linecolour = '#267f00';
+        if (xVal > parseInt({{scaler}} * {{pred[0]}})){linecolour = '#B40020'}
+        
+        // Draw another line if last one was over 20px away and not within 20px of the edge
+        if (lineLeftOffset > lastVertXPx + 25 && lineLeftOffset < xscale.right - 20){
+            // render vertical line
+            context.beginPath();
+            
+            context.strokeStyle = linecolour;
+            context.moveTo(lineLeftOffset, yscale.top);
+            context.lineTo(lineLeftOffset, yscale.bottom);
+            context.stroke();
+            
+            lastVertXPx = lineLeftOffset;
+            
+            // write label
+            context.fillStyle = linecolour;
+            context.textAlign = 'center';
+            context.fillText(text, lineLeftOffset, (yscale.bottom - yscale.top) / 2 + yscale.top);        
+        }
+    },
+    
+    afterDatasetsDraw: function (chart, easing) {
+        if (chart.config.lineAtxVal) {
+            chart.config.lineAtxVal.forEach(item => this.renderVerticalLine(chart, item[0], item[1]));
+        }
+    }
+};
 
+function update_vert_lines(achievement_type){
+    // select the set of vert lines
+    switch(achievement_type){
+    case 'cefr':    // default
+        vert_lines = [[500,"　 　A1"],[1000,"　 　A2"],[2000,"　 　B1"],[4000,"　 　B2"],[8000,"　 　C1"],[16000,"　 　C2"]];
+        break;
+    case 'grade':    
+        vert_lines = [[3000,"　 　Pre"],[4500,"　 　1"],[6000,"　 　2"],[7000,"　 　3"],[8000,"　 　4"],[9000,"　 　5"],[10000,"　 　6"],[10700,"　 　7"],[11500,"　 　8"],[12200,"　 　9"],[12900,"　 　10"],[13500,"　 　11"],[14000,"　 　12"]];
+        break;
+    case 'none':    
+    default:
+        vert_lines = []
+        break;        
+    }
+    lastVertXPx = 0;
+    mainChart.destroy();
+    mainctx = document.getElementById('predChart').getContext('2d');
+    mainChart = makeChart();
+};
+
+function makePrediction() {
+    var pred = []; 
+    for (var x = 0; x <= parseInt({{scaler}} * {{config['MAX_X']}}) + 50; x = x + 50) {
+        y = 1 / (1 + 2**({{t / scaler}} * (x - {{(a * scaler)|int}})))
+        pred.push({x: x, y: y});
+    }
+    return pred;
+};
+
+function rightPoints(val) {
+    return {x: parseInt({{scaler}} * val[0]), y: 1, question: val[1]};
+};
+
+function wrongPoints(val) {
+    return {x: parseInt({{scaler}} * val[0]), y: 0, question: val[1]};
+};
+
+var lastVertXPx = 0;
+var vert_lines = [[500,"　 　A1"],[1000,"　 　A2"],[2000,"　 　GB1"],[4000,"　 　B2"],[8000,"　 　C1"],[16000,"　 　C2"]];
 var prediction =  makePrediction();
-var ctx = document.getElementById('predChart').getContext('2d');
-var myLineChart = new Chart(ctx, {
+var mainctx = document.getElementById('predChart').getContext('2d');
+var mainChart = makeChart();
+
+function makeChart(){
+return new Chart(mainctx, {
     type: 'line',
     data: {
         datasets: [{
@@ -32,6 +108,8 @@ var myLineChart = new Chart(ctx, {
             pointRadius: 0
         }]
     },
+    plugins: [verticalLinePlugin],
+    lineAtxVal: vert_lines,
     options: {
         responsive: true,
         maintainAspectRatio: false,
@@ -85,26 +163,8 @@ var myLineChart = new Chart(ctx, {
             duration: 0
         }
     }
-});
+});};
 
-//console.log({{ rightanswers|safe }}.forEach(rightPoints))
-
-function makePrediction() {
-    var pred = []; 
-    for (var x = 0; x <= parseInt({{scaler}} * {{config['MAX_X']}}) + 50; x = x + 50) {
-        y = 1 / (1 + 2**({{t / scaler}} * (x - {{(a * scaler)|int}})))
-        pred.push({x: x, y: y});
-    }
-    return pred;
-};
-
-function rightPoints(val) {
-    return {x: parseInt({{scaler}} * val[0]), y: 1, question: val[1]};
-};
-
-function wrongPoints(val) {
-    return {x: parseInt({{scaler}} * val[0]), y: 0, question: val[1]};
-};
 
 </script>
 {% endblock chart %}
